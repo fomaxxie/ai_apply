b9a44a3c3cc395c1d51eb43637fde5f75ed2d285
Maksim Fomkin <fomaxxi@gmail.com>
2024-08-22 14:07:42 +0200

    improved Bio prompt

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -5,13 +5,15 @@ class Bio < ApplicationRecord
   validates :cv_content, presence: true
 
   def ai_bio_output
-    prompt = "you are the best hiring manager that ever existed and you want to give the best
-    advice on how to write a bio which will summarize the candidate's experience and skills from
-    their CV. You want to make sure that the candidate's bio is written in a way that will make
-    them stand out from the crowd and get the job they want.
-    Here is their CV: #{cv_content}. Make sure to also take into account the following details:
-    #{details}. The candidate's bio should consider that they are applying for the position of
-    #{profile.desired_position} and that they have #{profile.years_of_experience} years of experience."
+    prompt = "As an expert hiring manager, provide a compelling bio for a candidate
+    based on the CV and specific requirements listed below. This bio should succinctly
+    summarize their experience and skills to make them an outstanding candidate for their
+    desired position.
+    CV Overview: #{cv_content}
+    Additional Details: #{details}
+    Target Position: #{profile.desired_position}
+    Years of Experience: #{profile.years_of_experience} years.
+    The bio should be concise, professional, and tailored to the target role."
 
 
     client = OpenAI::Client.new
